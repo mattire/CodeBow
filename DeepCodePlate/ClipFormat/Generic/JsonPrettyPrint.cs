fc8dd0a541db90543c311c2d5b8ceda337d4b8ee
mattire <alr2d4@hotmail.com>
2021-02-06 20:22:04 +0200

    add newtonsoft, add json pretty print formatter

---
+++ DeepCodePlate/ClipFormat/Generic/JsonPrettyPrint.cs
@@ -0,0 +1,21 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodingHood.ClipFormat.Generic
+{
+    class JsonPrettyPrint : IClipFormatter
+    {
+        
+        public string Name { get { return "JsonPrettyPrintFormatter"; } }
+
+        public string Format(string str)
+        {
+            dynamic parsedJson = JsonConvert.DeserializeObject(str);
+            return JsonConvert.SerializeObject(parsedJson, Formatting.Indented);
+        }
+    }
+}
